8b8f54cd5e059933de3f678bc8e852992d748385
Guojingye <guojingye@banggood.com>
2019-06-15 11:56:46 +0800

    init project

---
+++ src/main/java/com/chowi/gydmall/GydMallApplication.java
@@ -0,0 +1,13 @@
+package com.chowi.gydmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GydMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GydMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chowi/gydmall/GydMallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chowi.gydmall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GydMallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
