832a56b562446d17ac2b5aa9ffdb4eec0a892ae5
Guojingye <guojingye@banggood.com>
2019-06-17 11:25:59 +0800

    update controller

---
+++ src/main/java/com/chowi/gydmall/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.chowi.gydmall.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String index(){
+        return "hi,Jenkins";
+    }
+}
